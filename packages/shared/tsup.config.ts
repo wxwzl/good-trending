2143a2f07da8f262d5b486b093a23cff44c16849
wxwzl <zgjc456@163.com>
2026-03-09 20:42:57 +0800

    chore: share 包代码

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: {
     index: "src/index.ts",
-    types: "src/types/index.ts",
-    constants: "src/constants/index.ts",
-    utils: "src/utils/index.ts",
   },
   format: ["cjs", "esm"],
   dts: true,
